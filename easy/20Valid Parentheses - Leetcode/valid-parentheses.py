feca9dafbddd80a22af04432e705182f8d7710bc
atommnava <atommunoznava@gmail.com>
2025-09-19 18:53:19 -0600

    Valid Parentheses v1

--- easy/20Valid Parentheses - Leetcode/valid-parentheses.py
+++ easy/20Valid Parentheses - Leetcode/valid-parentheses.py
@@ -4,5 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = "()"
-        if s
+        hashmap = {')':'(', '}':'{', ']':'['}
+        stack = []
+        # 'i' is our closing bracket
+        for i in s:
+            # It means we have an open bracket
+            if i not in hashmap:
+                stack.append(i)
+            # It means we have a closing bracket
+            else:
+                if not stack:
+                    return False
+                else:
+                    # Take off the most recent open bracket on the stack
+                    popped = stack.pop()
+                    if popped != hashmap[i]:
+                        return False
+        # To make sure we do not have a stack
+        return not stack
+
+
+
+
