68474fde671f1171e49a286f6221d6fa2772c588
atommnava <atommunoznava@gmail.com>
2025-10-28 16:05:07 -0600

    Changes

---
+++ medium/146LRU Cache/lru_cache.py
@@ -0,0 +1,35 @@
+from Collections import OrderedDict
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self:
+            self.move_to_end(key)
+            return self[key]
+        else:
+            return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        self[key] = value
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
