7c421d41dfaa26a39454bbc546ede7fd980f2f6a
atommnava <atommunoznava@gmail.com>
2025-10-28 14:53:54 -0600

    Changes

---
+++ medium/937Reorder Data In Log Files/reorder_data.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        digit_logs = []
+        letter_logs = []
+        # go through every log, put digit logs into digit_logs, same with letters
+        for log in logs:
+            if log[-1].isdigit():
+                digit_logs.append(log)
+            else:
+                letter_logs.append(log)
+        # sort letter_logs first by contents, then by identifiers
+        letter_logs.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        result = letter_logs + digit_logs
+        return result
